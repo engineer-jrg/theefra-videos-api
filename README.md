53503f4a83a3ede4fbf2dda5765e3de38156782a
Juan Rodriguez Gasca <engineer.jrg@gmail.com>
2020-05-07 21:11:02 -0500

    :rocket: compromiso inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+## TheEfra Videos - API
+---
+Una api simple que permite solicitar datos del canal de youtube [TheEfra](https://www.youtube.com/channel/UCC7y8uQQXtSxXDGle76HAxw)
--- index.js
+++ index.js
@@ -10,12 +10,14 @@ app.use(morgan('tiny'));
 app.use(cors());
 
 app.use('/videos', (req, res) => {
-  const url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLbDLkhJ5sFvCWFbP4tAFALHkNWNFo_FiL&masResult=50';
+  // const url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLqhmS_S1oOPHMTxA6Ms67C29XMnz-HbEw&maxResults=50';
+  const url = 'https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCC7y8uQQXtSxXDGle76HAxw&maxResults=50';
   fetch(`${url}&key=${process.env.GOOGLE_API_KEY}`)
     .then((response) => response.json())
     .then((json) => {
-      res.json(json);
-    });
+      res.json(json.items);
+    })
+    .catch((error) => console.log(error) );
 });
 
 function notFound(req, res, next) {
